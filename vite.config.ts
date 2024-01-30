848fc26bef525bc21af3975be15409dcbb8a92dc
dumster <b.abduvaliyev@igenius.uz>
2024-01-30 14:29:00 +0500

    add header to req

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/auth-back': {
+        target: 'https://usnc.dev-webdevep.ru',
+        changeOrigin: true,
+      },
+    },
+  },
 })
